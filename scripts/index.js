cb28a046f99ee76cca4e4c618987367a5b57216b
Siddhart <Siddhart_7@live.nl>
2020-12-17 12:40:29 +0100

    some mobile responsiveness
    some mobile responsiveness

--- scripts/index.js
+++ scripts/index.js
@@ -6,15 +6,15 @@ var DidCount = false
 document.getElementById("page").onscroll = function() {
     if(DidCount == false){
         DidCount = true
-        document.getElementById('SETUP').innerHTML = '<b id="USERS" class="counter" data-duration="2500">1500000</b>+ users use our services/bots?</p>'
+        document.getElementById('SETUP').innerHTML = '<b id="USERS" class="counter" data-duration="1750">2000000</b>+ users use our services/bots?</p>'
         COUNT()
         setTimeout(() => {
             REPLACE()
-        }, 2500);
+        }, 1750);
 
     }
 };
 
 function REPLACE(){
-    document.getElementById('SETUP').innerHTML = '<b>1.500.000</b>+ users use our services/bots?</p>'
+    document.getElementById('SETUP').innerHTML = '<b>2.000.000</b>+ users use our services/bots?</p>'
 }
